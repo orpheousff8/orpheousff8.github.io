f126043049ad33d278188bc0df54937bb8b54c48
Saracha <orpheousff8@gmail.com>
2020-09-12 13:37:50 +0700

    react lab3

--- lab-homeWork/react/react/lab3-jsx/src/App.js
+++ lab-homeWork/react/react/lab3-jsx/src/App.js
@@ -1,30 +1,35 @@
 import React from 'react';
 import './App.css';
 
-function Saracha(props) {
+function MyTag(props) {
 
   let name = props.name;
   let age = props.age;
   let isMan = props.isMan;
+  let theNull = props.theNull;
+  let theUndefined = props.theUndefined;
   let location = props.location;
   let education = props.education;
 
   return (
   <div>
-    My name is: {name}<br/>
-    My age is: {age}<br/>
-    I am a {isMan ? "Man" : "Woman"}.<br/>
-    <ul>My place(s):
+    <strong>Test string:</strong> <p>{name}</p>
+    <strong>Test number:</strong> <p>{age}</p>
+    <strong>Test boolean:</strong> <p>{isMan}</p>
+    <strong>Test boolean with a condition:</strong> <p>{isMan ? "True" : "False"}</p>
+    <strong>Test null:</strong> <p>{theNull}</p>
+    <strong>Test undefined:</strong> <p>{theUndefined}</p>
+    <ul style={{paddingLeft:"0"}}><strong>Test string array:</strong>
       {
         location.map((place) => (
-          <li>At: {place}</li>
+          <li style={{paddingLeft:"1rem"}}>string: {place}</li>
         ))
       }
     </ul>
-    <ul>My education:
+    <ul style={{paddingLeft:"0"}}><strong>Test object:</strong>
       {
         Object.keys(education).map((level, index) => (
-          <li>Level: {level}, Where: {education[level]}</li>
+          <li style={{paddingLeft:"1rem"}}>key: {level}, value: {education[level]}</li>
         ))
       }
     </ul>
@@ -34,7 +39,8 @@ function Saracha(props) {
 
 function App() {
   return (
-    <Saracha name="Saracha" age={6} isMan={true} location={["abc", "def"]} education={{"M6":"School", "bachelor":"Uni"}}/>
+    <MyTag name="Dio" age={6} isMan={true} theNull={null} theUndefined={undefined} 
+    location={["abc", "def"]} education={{"Grade12":"School", "bachelor":"Uni"}}/>
   );
 }
 
